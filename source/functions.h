12e4bef050351bc104608d079a3d461f7e1f8a10
wieselsoft <stadtmuellertim@gmail.com>
2015-08-19 18:30:57 +0200

    Formed class-declaration around functions.h

--- source/functions.h
+++ source/functions.h
@@ -20,36 +20,32 @@ This file is part of PiLitez.
 
 #define SIZE 8
 
-void randomLights( int cnt );
-
-void dualLights2( int cnt );
-void revDual( int cnt );
-
-void outIn2();
-void inOut2();
-
-void allOn();
-void allOf();
-
-void knightRiderLights( int cnt );
-
-void pattern1( long int millis = 400 );
-void revPattern1( long int millis = 400 );
-
-void pattern2( long int millis = 400 );
-void revPattern2( long int millis = 400 );
-
-void pattern3();
-void revPattern3();
-
-void strobe( int cnt );
-void greenStrobe( int cnt );
-void blueStrobe( int cnt );
-void yellowStrobe( int cnt );
-void redStrobe( int cnt );
-
-void vu( int cnt );
-
-void music();
+class NoMusic
+{
+  public:
+    NoMusic( int pins[] );
+    void randomLights( int cnt );
+    void dualLights2( int cnt );
+    void revDual( int cnt );
+    void outIn2();
+    void inOut2();
+    void allOn();
+    void allOf();
+    void knightRiderLights( int cnt );
+    void pattern1( long int millis = 400 );
+    void revPattern1( long int millis = 400 );
+    void pattern2( long int millis = 400 );
+    void revPattern2( long int millis = 400 );
+    void pattern3();
+    void revPattern3();
+    void strobe( int cnt );
+    void greenStrobe( int cnt );
+    void blueStrobe( int cnt );
+    void yellowStrobe( int cnt );
+    void redStrobe( int cnt );
+    void vu( int cnt );
+  private:
+    int pins[];
+};
 
 #endif
