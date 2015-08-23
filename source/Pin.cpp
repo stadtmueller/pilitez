3f09d1f30179d71e5c1e88e3a163fea6d5427fe0
wieselsoft <stadtmuellertim@gmail.com>
2015-08-23 02:27:24 +0200

    Improved initPins() in Pin.h

--- source/Pin.cpp
+++ source/Pin.cpp
@@ -25,6 +25,9 @@ void initPins( int pins[] )
   wiringPiSetup();
   system( "gpio edge 29 rising" );
 
+  for( int i : pins )
+    pinMode( i, OUTPUT );          // <-- Add "Pin"-call here and buffer it in array
+
   cout << "Init done" << endl << endl;
 }
 
