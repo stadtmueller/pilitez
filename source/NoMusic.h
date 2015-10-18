c6f1056c04055b441e04b99ead60dbdb945c1983
wieselsoft <stadtmuellertim@gmail.com>
2015-10-18 23:40:49 +0200

    Added class concept to NoMusic.h

--- source/NoMusic.h
+++ source/NoMusic.h
@@ -18,25 +18,34 @@ This file is part of PiLitez.
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-void randomLights( int cnt );
-void dualLights2( int cnt );
-void revDual( int cnt );
-void outIn2();
-void inOut2();
-void allOn();
-void allOf();
-void knightRiderLights( int cnt );
-void pattern1( long int millis = 400 );
-void revPattern1( long int millis = 400 );
-void pattern2( long int millis = 400 );
-void revPattern2( long int millis = 400 );
-void pattern3();
-void revPattern3();
-void strobe( int cnt );
-void greenStrobe( int cnt );
-void blueStrobe( int cnt );
-void yellowStrobe( int cnt );
-void redStrobe( int cnt );
-void vu( int cnt );
+#include <vector>
+
+class NoMusic
+{
+  public:
+    NoMusic( std::vector<int> pins_ );
+    void randomLights( int cnt );
+    void dualLights2( int cnt );
+    void revDual( int cnt );
+    void outIn2();
+    void inOut2();
+    void allOn();
+    void allOf();
+    void knightRiderLights( int cnt );
+    void pattern1( long int millis = 400 );
+    void revPattern1( long int millis = 400 );
+    void pattern2( long int millis = 400 );
+    void revPattern2( long int millis = 400 );
+    void pattern3();
+    void revPattern3();
+    void strobe( int cnt );
+    void greenStrobe( int cnt );
+    void blueStrobe( int cnt );
+    void yellowStrobe( int cnt );
+    void redStrobe( int cnt );
+    void vu( int cnt );
+  private:
+    std::vector<int> pins;
+};
 
 #endif
